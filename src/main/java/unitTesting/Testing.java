b4cf56bfa2418cb8944198edce45604d85921061
Raheem Shaik <raheem.shaik@inmar.com>
2021-07-05 23:45:36 +0530

    first commit

---
+++ src/main/java/unitTesting/Testing.java
@@ -0,0 +1,18 @@
+package unitTesting;
+
+/**
+ * @author Raheem-PC
+ * @created 01/07/2021
+ * @project github-maven-packages-example
+ */
+public class Testing {
+
+    public static void display(String param){
+        System.out.println("You parameter is " + param);
+    }
+
+    public void someFunc(){
+        System.out.println("unitTesting.Testing.someFunc");
+    }
+}
+
